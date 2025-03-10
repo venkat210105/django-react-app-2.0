34352bae3af74f7502f9add8069edc71f8b178b1
venkat <venkatmariserla21@gmail.com>
2025-03-10 20:22:58 +0530

    1.52

--- django_react_project/django_react_project/settings.py
+++ django_react_project/django_react_project/settings.py
@@ -133,3 +133,6 @@ LOGGING = {
         'level': 'INFO',
     },
 }
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
--- django_react_project/plant_App/urls.py
+++ django_react_project/plant_App/urls.py
@@ -3,7 +3,7 @@ from django.views.generic import TemplateView
 from .views import signup, login, me, predict, update_password, update_email, update_mobile
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='index.html')),
+    #path('', TemplateView.as_view(template_name='index.html')),
     path('signup/', signup, name='signup'),
     path('login/', login, name='login'),
     path('me/', me, name='me'),
